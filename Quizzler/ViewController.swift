6ed7754676bade7c8dffd940a4fdf77b4725a9d6
Shaleen, Adam (ETW - FLEX) <Adam.Shaleen@nike.com>
2019-03-06 15:17:36 -0800

    initial commit

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,43 +11,56 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
+    let allQuestions = QuestionBank()
+    var pickedAnswer: Bool = false
+    var questionNumber: Int = 0
     
-    
-    @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var question: UILabel!
+    @IBOutlet weak var score: UILabel!
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        question.text = firstQuestion.questionText
     }
-
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        pickedAnswer = (sender.tag == 1) ? true : false
+        checkAnswer()
+        questionNumber += 1
+        nextQuestion()
     }
-    
     
     func updateUI() {
       
     }
     
-
     func nextQuestion() {
-        
+        if questionNumber <= 12 {
+            question.text = allQuestions.list[questionNumber].questionText
+        }
+        else {
+            let alert = UIAlertController(title: "All Done!", message: "You finished the Quiz", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Restart", style: .default) {
+                (UIAlertAction) in self.startOver()
+            }
+            
+            alert.addAction(restartAction)
+            present(alert, animated: true, completion: nil)
+        }
     }
-    
     
     func checkAnswer() {
-        
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        print(pickedAnswer == correctAnswer ? "You got it!" : "Wrong")
     }
-    
     
     func startOver() {
-       
+       questionNumber = 0
+        nextQuestion()
     }
-    
-
-    
 }
